219c6ff37fbd415740ae44707fc0e7a3f55f5691
Lyan Jover <138700965+GrdnhffMann@users.noreply.github.com>
2023-08-25 22:39:42 +0800

    Added comments for every function

--- C-Programming-Files/Arrays-in-Struct-Delete-Instance.c
+++ C-Programming-Files/Arrays-in-Struct-Delete-Instance.c
@@ -6,6 +6,10 @@ typedef struct node {
 	int count;
 }LIST;
 
+/* An array is inside a struct with a size of 8 elements
+ The count variable in the struct determines the
+ actual number of elements, not the overall */
+
 void insertLast(char element, LIST *main);
 void displayListArr(LIST *main);
 void removeInstance(char element, LIST *main);
@@ -30,13 +34,16 @@ void insertLast(char element, LIST *main) {
 	int i;
 	if (main->count < MAX) {
 		for (i = 0; i < main->count; i++) {}
+        //for loop to traverse to the end
 		(i == main->count) ? main->elem[main->count++] = element : 0;
+        //checks if the iterator has the same value as the count
 	}
 }
 
 void displayListArr(LIST *main) {
 	int i;
 	for (i = 0; i < main->count; printf("%c ", main->elem[i]), i++) {}
+    //a lot more line-efficient way to print it all
 	printf("\n");
 }
 
@@ -46,8 +53,12 @@ void removeInstance(char element, LIST *main) {
 		j = i;
 		if (main->elem[i] == element) {
 			 main->count--;
+             //decrement for every similar element encountered in the array
 			 for (; j < main->count; main->elem[j] = main->elem[j+1], ++j) {}
+             //shift right elements to the current position
+             //its basically overwriting the elements
 			 main->elem[main->count] = 0;
+             //last element set to 0
 		} else {
 			++i;
 		}
