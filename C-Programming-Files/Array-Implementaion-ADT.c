ee4c03f910481ea1af7b2af09cd288632ff5fdf1
Lyan Jover <138700965+GrdnhffMann@users.noreply.github.com>
2023-08-31 10:29:10 +0800

    Update Array-Implementaion-ADT.c

--- C-Programming-Files/Array-Implementaion-ADT.c
+++ C-Programming-Files/Array-Implementaion-ADT.c
@@ -3,35 +3,54 @@
 #define MAX 8
 
 typedef struct node {
-	char *ptr;
-	int count;
+    char *ptr;
+    int count;
 }List;
 
 List init();
 void insertFrontUnique(char data, List *main);
+void removeInstance(char data, List *main);
 
 int main () {
-	
-	List main = init();
-	insertFrontUnique('A', &main);
-	insertFrontUnique('A', &main);
-	insertFrontUnique('B', &main);
-	
-	return 0;
+    
+    List main = init();
+    insertFrontUnique('A', &main);
+    insertFrontUnique('A', &main);
+    insertFrontUnique('B', &main);
+    insertFrontUnique('D', &main);
+    insertFrontUnique('F', &main);
+    insertFrontUnique('G', &main);
+    insertFrontUnique('F', &main);
+    insertFrontUnique('Y', &main);
+    insertFrontUnique('Z', &main);
+    removeInstance('B', &main);
+    
+    return 0;
 }
 
 List init() {
-	List start = {
-		.ptr = (char*)malloc(MAX),
-		.count = -1
-	};
-	return start;
+    List start = {
+        .ptr = (char*)calloc(MAX, 1),
+        .count = -1
+    };
+    return start;
 }
 
 void insertFrontUnique(char data, List *main) {
-	int i;
-	for (i = 0; i <= main->count && main->ptr[i] != data; ++i) {}
-	if (i <= MAX && i > main->count) {
-		main->ptr[++main->count] = data;
-	}
+    int i;
+    for (i = 0; i <= main->count && main->ptr[i] != data; ++i) {}
+    if (i < MAX && i > main->count) {
+        main->ptr[++main->count] = data;
+    }
+}
+
+void removeInstance(char data, List *main) {
+    int i, j;
+    for (i = 0; i < main->count; ++i) {
+        j = i;
+        if (main->ptr[i] == data) {
+            for (; j < main->count; main->ptr[j] = main->ptr[j+1], ++j) {}
+            main->ptr[main->count--] = '\0';
+        }
+    }
 }
