ba85ba3f942eb1ddc2ebeba3cc8a9a0e07753741
Lyan Jover <138700965+GrdnhffMann@users.noreply.github.com>
2023-09-02 15:38:15 +0800

    Update DSA-Tutorial-Activity-1.c
    Included a new Function. Sorting capabilities

--- C-Programming-Files/DSA-Tutorial-Activity-1.c
+++ C-Programming-Files/DSA-Tutorial-Activity-1.c
@@ -11,12 +11,13 @@ typedef enum {FALSE, TRUE} Boolean;
 /*** enum is short for enumeration ***************************
 ******* kinda like structs **********************************/
 Boolean findInt(ArrayList *L, int data);
+Boolean findInsertSorted(ArrayList *L, int data);
 /** return true if x is in the given array, false if nope **/
 
 int main () {
 	
 	ArrayList main = {{1,2,3,4,5,6}, 6};
-	Boolean check = findInt(&main, 10);
+	Boolean check = findInsertSorted(&main, 10); 
 	(check == 1) ? printf("No data was found!\nNew data was inserted!") : printf("Gothca!") ;
 	
 	return 0;
@@ -27,20 +28,10 @@ int main () {
 //	int ndx;
 //	for (ndx = 0; ndx < L.count && L.arr[ndx] != data; ++ndx) {}
 //	return (ndx == L.count)? FALSE : TRUE; //one line ternary return
+//}rn flag;
 //}
 
-//Boolean findInt(ArrayList *L, int data) {
-//	Boolean flag = TRUE;
-//	if (L->count < SIZE) {
-//		int ndx;
-//		for (ndx = 0; ndx < L->count && L->arr[ndx] != data; ++ndx) {}
-//		(ndx == L->count) ? (flag = TRUE), L->arr[ndx] = data : (flag = FALSE);
-//		++L->count;
-//	}
-//	return flag;
-//}
-
-Boolean findInt(ArrayList *L, int data) {
+Boolean findIntInsert(ArrayList *L, int data) {
 	Boolean retVal;
 	if (L->count < SIZE) {
 		int ndx;
@@ -50,3 +41,23 @@ Boolean findInt(ArrayList *L, int data) {
 	}
 	return retVal;
 }
+
+Boolean findInsertSorted(ArrayList *L, int data) {
+	Boolean retVal;
+	if (L->count < SIZE) {
+		int ctr;
+		for (ctr = 0; ctr < L->count && L->arr[ctr] != data; ++ctr) {}
+		if (ctr == L->count) {
+			for (; ctr > 0 && L->arr[ctr-1] > data; L->arr[ctr] = L->arr[ctr-1], --ctr) {}
+			L->arr[ctr] = data;
+			L->count++;
+			retVal = TRUE;
+		} else {
+			retVal = FALSE;
+		}
+	}
+	return retVal;
+}
+
+//ArrayList *L= (ArrayList*)calloc(1, sizeof(ArrayList));
+//L->arr[5] = {1,2,3,4,5};
