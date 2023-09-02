95439b96e9af831532e2a31e655bcc6e01779a34
Lyan Jover <138700965+GrdnhffMann@users.noreply.github.com>
2023-09-02 14:15:28 +0800

    Create DSA-Tutorial-Activity-1.c

---
+++ C-Programming-Files/DSA-Tutorial-Activity-1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define SIZE 10
+
+typedef struct node {
+	int arr[SIZE], count;
+}ArrayList;
+
+/*************** enum definition *****************************/
+typedef enum {FALSE, TRUE} Bool;
+/*** enum is short for enumeration ***************************
+******* kinda like structs **********************************/
+Bool findInt(ArrayList L, int x);
+/** return true if x is in the given array, false if nope **/
+
+int main () {
+	
+	ArrayList main = {{1,2,3,4,5,6}, 6};
+	Bool check = findInt(main, 3);
+	(check == 1) ? printf("We found it!") : printf("Looks like it is not here!") ;
+	
+	return 0;
+}
+
+Bool findInt(ArrayList L, int x) {
+	Bool check;
+	int i;
+	for (i = 0; i < L.count && L.arr[i] != x; ++i) {}
+	(i == L.count) ? (check = FALSE) : (check = TRUE);
+	return check;
+}
