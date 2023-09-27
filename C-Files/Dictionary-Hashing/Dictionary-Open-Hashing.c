7efd8300c2d740791fbfddf3ab949a1b888f01c9
GrdnhffMann <138700965+GrdnhffMann@users.noreply.github.com>
2023-09-28 00:57:45 +0800

    Added Hashing Function, Insert, Print

--- C-Files/Dictionary-Hashing/Dictionary-Open-Hashing.c
+++ C-Files/Dictionary-Hashing/Dictionary-Open-Hashing.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define SIZE 8
 
+typedef enum {FALSE, TRUE} Boolean;
+
 typedef struct nodes {
     int data;
     struct nodes *next;
@@ -9,11 +11,71 @@ typedef struct nodes {
 
 typedef List Dictionary[SIZE];
 
-
+void initHTable(Dictionary table);
+int hashFunction(int elem);
+Boolean isMember(Dictionary table, int elem);
+void insertHash(Dictionary table, int elem);
+void displayHTable(Dictionary table);
 
 int main () {
 
     Dictionary main;
+    initHTable(main);
+
+    insertHash(main, 1);
+    insertHash(main, 5);
+    insertHash(main, 4);
+    insertHash(main, 1);
+    insertHash(main, 2);
+    insertHash(main, 11);
+    insertHash(main, 23);
+    insertHash(main, 99);
+
+    displayHTable(main);
 
     return 0;
+}
+
+void initHTable(Dictionary table) {
+    int i;
+    for (i = 0; i < SIZE; ++i) {
+        table[i] = NULL;
+    }
+}
+
+int hashFunction(int elem) {
+    return elem % SIZE;
+}
+
+Boolean isMember(Dictionary table, int elem) {
+    int pos = hashFunction(elem);
+    List trav;
+    for (trav = table[pos]; trav != NULL && trav->data != elem; trav = trav->next) {}
+    return (trav != NULL) ? TRUE : FALSE ;
+}
+
+void insertHash(Dictionary table, int elem) {
+    if (!isMember(table, elem)) {
+        int pos = hashFunction(elem);
+        List temp = (List)malloc(sizeof(struct nodes));
+        if (temp != NULL) {
+            temp->data = elem;
+            temp->next = table[pos];
+            table[pos] = temp;
+        }
+    } else {
+        printf("\nNot unique!\n");
+    }
+}
+
+void displayHTable(Dictionary table) {
+    int ndx;
+    List trav;
+    for (ndx = 0; ndx < SIZE; ++ndx) {
+        printf("[%d] => ", ndx);
+        for (trav = table[ndx]; trav != NULL; trav = trav->next) {
+            (trav->next != NULL) ? printf("(%d) -> ", trav->data) : printf("(%d)", trav->data);
+        }
+        printf("\n");
+    }
 }
